cebf04de0bb71bb91f52c16c34d158fe376100dc
Luka929-del <luka.natsvlishvili.1@btu.edu.ge>
2025-05-30 21:10:39 +0400

    wrote some code in js
    I wrote some code and solved the Exercise was given

---
+++ script.js
@@ -0,0 +1,7 @@
+const array = [10, 20, 30, 40, 50];
+const valueToFind = 30;
+if (array.includes(valueToFind) == true){
+    console.log('number is found')
+} else {
+    console.log('number not found')
+}
